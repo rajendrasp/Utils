4195f7fa64760d1c91d475e0769693796158cfc5
Rajendrasing Pardeshi <rpardesh@microsoft.com>
2023-06-13 13:16:10 +0530

    removing logging per thread

--- mytracing.h
+++ mytracing.h
@@ -5,10 +5,6 @@
 #include <stdio.h>
 #include <thread>
 #include <sstream>
-#include <unordered_map>
-
-#include "D:\Utils\Utils\plog\Log.h"
-#include "D:\Utils\Utils\plog\Initializers\RollingFileInitializer.h"
 
 inline auto Trace(wchar_t const* format, ...) -> void
 {
@@ -27,132 +23,29 @@ inline auto Trace(wchar_t const* format, ...) -> void
     OutputDebugString(buffer);
 }
 
-enum ThreadsID
-{
-    One = 1,
-    Two,
-    Three,
-    Four,
-    Five,
-    Six,
-    Seven,
-    Eight,
-    Nine,
-    Ten,
-    Eleven
-
-};
-
-int N = 10;
-
 struct Tracer
 {
     char const* m_filename;
     unsigned m_line;
     char const* m_functionname;
-    static std::unordered_map<std::string, int> m_threads;
-    static int m_counter;
 
     Tracer(char const* filename, unsigned const line, char const* functionname) :
         m_filename{ filename },
         m_line{ line },
         m_functionname{ functionname }
     {
-    }
 
-    void createLogFile() const
-    {
-        std::string str = "log_" + std::to_string(m_counter) + ".txt";
-        switch (m_counter)
-        {
-        case 0:
-            plog::init(plog::debug, str.c_str());
-            break;
-        case 1:
-            plog::init<ThreadsID::One>(plog::debug, str.c_str());
-            break;
-        case 2:
-            plog::init<ThreadsID::Two>(plog::debug, str.c_str());
-            break;
-        case 3:
-            plog::init<ThreadsID::Three>(plog::debug, str.c_str());
-            break;
-        case 4:
-            plog::init<ThreadsID::Four>(plog::debug, str.c_str());
-            break;
-        case 5:
-            plog::init<ThreadsID::Five>(plog::debug, str.c_str());
-            break;
-        case 6:
-            plog::init<ThreadsID::Six>(plog::debug, str.c_str());
-            break;
-        case 7:
-            plog::init<ThreadsID::Seven>(plog::debug, str.c_str());
-            break;
-        case 8:
-            plog::init<ThreadsID::Eight>(plog::debug, str.c_str());
-            break;
-        case 9:
-            plog::init<ThreadsID::Nine>(plog::debug, str.c_str());
-            break;
-        case 10:
-            plog::init<ThreadsID::Ten>(plog::debug, str.c_str());
-            break;
-        }
-    }
-
-    void logThis(wchar_t const* buffer, int logger)
-    {
-        switch (logger)
-        {
-        case 0:
-            PLOGD << buffer;
-            break;
-        case 1:
-            PLOGD_(ThreadsID::One) << buffer;
-            break;
-        case 2:
-            PLOGD_(ThreadsID::Two) << buffer;
-            break;
-        case 3:
-            PLOGD_(ThreadsID::Three) << buffer;
-            break;
-        case 4:
-            PLOGD_(ThreadsID::Four) << buffer;
-            break;
-        case 5:
-            PLOGD_(ThreadsID::Five) << buffer;
-            break;
-        case 6:
-            PLOGD_(ThreadsID::Six) << buffer;
-            break;
-        case 7:
-            PLOGD_(ThreadsID::Seven) << buffer;
-            break;
-        case 8:
-            PLOGD_(ThreadsID::Eight) << buffer;
-            break;
-        case 9:
-            PLOGD_(ThreadsID::Nine) << buffer;
-            break;
-        case 10:
-            PLOGD_(ThreadsID::Ten) << buffer;
-            break;
-        }
     }
 
     template <typename... Args>
-    auto operator()(wchar_t const* format, Args... args) -> void
+    auto operator()(wchar_t const* format, Args... args) const -> void
     {
         wchar_t buffer[256];
 
-        //std::thread::id this_id = std::this_thread::get_id();
-        //std::stringstream ss;
-        //ss << this_id;
-        //std::string thread_id = ss.str();
-
-        int myint = rand() % N;
-        std::string thread_id = std::to_string(myint);
+        std::thread::id this_id = std::this_thread::get_id();
+        std::stringstream ss;
+        ss << this_id;
+        std::string thread_id = ss.str();
 
         auto count = swprintf_s(buffer,
             L"DEBUG_TRACING Thread: %S %S(%d)%S: ",
@@ -169,26 +62,9 @@ struct Tracer
             format,
             args...);
 
-
-        auto iter = m_threads.find(thread_id);
-        if (iter != m_threads.end())
-        {
-            logThis(buffer, iter->second);
-        }
-        else
-        {
-            createLogFile();
-            m_threads[thread_id] = m_counter;
-            logThis(buffer, m_counter);
-            m_counter++;
-        }
-
         OutputDebugString(buffer);
     }
 };
-
-int Tracer::m_counter = 0;
-std::unordered_map<std::string, int> Tracer::m_threads;
 
 #define MYOWNTRACEMINE Tracer(__FILE__, __LINE__, __FUNCTION__)
 
